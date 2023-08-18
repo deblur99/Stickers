29002013010fe36eb5f92e3671e7650ea21d1a68
deblur99 <deblur99@kakao.com>
2023-08-18 09:14:24 +0900

    Initial Commit

---
+++ Stickers/StickersApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickersApp.swift
+//  Stickers
+//
+//  Created by 한현민 on 2023/08/18.
+//
+
+import SwiftUI
+
+@main
+struct StickersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
